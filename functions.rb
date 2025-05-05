6bda0f967fdc52a5efada52c6c28888b28c43c07
usmandevlpr <cu2948528@gmail.com>
2025-05-06 01:29:38 +0500

    Initial commit

---
+++ functions.rb
@@ -0,0 +1,24 @@
+# Functions 
+
+
+def func value
+  puts value
+end
+func 4
+
+
+
+
+def function value
+  val =30
+  puts value
+end 
+puts val  #/ it not work b/e of local scope of var
+
+
+
+#/ function to generate a random no
+def random 
+  puts rand(1..100)
+end
+random
