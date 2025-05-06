904d04b92749007cc428ecd0965685c8f6067679
usmandevlpr <cu2948528@gmail.com>
2025-05-06 16:15:44 +0500

    Initial commit with ruby files

---
+++ scratch.rb
@@ -0,0 +1,35 @@
+
+puts "Hello, Ruby!"
+puts 2
+puts 20%2
+
+
+# how many hours are in a year?
+puts 365*24
+
+
+
+puts 'blink'* 2
+puts 'Fred' + 'John'
+
+
+
+# tointeger
+var1 = '2'
+var2 = 5
+
+puts var1 + var2.to_s
+puts var1.to_i + var2
+puts '3 Who asked you about 5 or whatever?'.to_i
+puts ' is 3 my favorite number?'.to_i
+
+
+# get user data
+name = gets.chomp
+
+
+# capitalize
+puts 'Hello, ' + name + 'gr'.capitalize + '!'
+
+
+
---
+++ time.rb
@@ -0,0 +1,7 @@
+time =Time.new
+puts time.to_s.split[0]
+puts time.to_s.split[1]
+puts time.to_s.split[2]
+
+
+
