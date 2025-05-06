594be5419c64c53fecd03c016068b7002c54b5a0
usmandevlpr <cu2948528@gmail.com>
2025-05-06 19:05:34 +0500

    commit some changes

--- loops.rb
+++ loops.rb
@@ -1,46 +1,93 @@
 
-#LOOPS
+# #LOOPS
 
-# 1. while loop
-    i = 0
-    while i <= 5
-      # puts i
-      i+=1
-    end
+# # 1. while loop
+#     i = 0
+#     while i <= 5
+#       puts i
+#       i+=1
+#     end
 
-#until loop
-i=3
-until i>4
-  # puts i
-  i+=1
-end
-
-
-# for loop
-for i in 1..6
-  # puts i 
-end
+# #until loop
+# i=3
+# until i>4
+#   puts i
+#   i+=1
+# end
 
 
-#times loop
-3.times do |i|
-  # puts i
-end
+# # for loop
+# for i in 1..6
+#   # puts i 
+# end
+
+
+# #times loop
+# 3.times do |i|
+#   puts i
+# end
+
+# 3.times do 
+#   puts 'i'
+# end
+
+
+
+
+# userdata=gets
+# while userdata!="exit"
+#  puts "you write "+ userdata
+#  userdata =gets.chomp
+#  if userdata=='yes'
+#   puts "completed"
+# end
+# end
+
+
+
+# do while loop
+# num = 0
+# begin
+#   print "Enter a number (type 0 to stop): "
+#   num = gets.chomp.to_i
+#   puts "You entered: #{num}"
+# end while num != 0
+
+
+
+# # do while loop with condition check
+# num = 0
+# begin
+#   print "Enter a number (type 0 to stop): "
+#   num = gets.chomp.to_i
+#   puts "You entered: #{num}"
+# end while num != 0
+
+
 
 3.times do 
-  # puts 'i'
+  puts "jao"
+end 
+
+
+for i in 1..3
+  puts "KK"
 end
 
 
 
+puts "Enter a number:"
+num = gets.chomp.to_i
 
-userdata=gets
-while userdata!="exit"
- puts "you write "+ userdata
- userdata =gets.chomp
- if userdata=='yes'
-  puts "completed"
-end
+while num <= 20
+  if num < 20
+    puts "Less than 20"
+  else
+    puts "Equal to 20"
+  end
+  puts "Increasing number..."
+  num += 3
 end
 
+puts "Now the number is greater than 20"
 
